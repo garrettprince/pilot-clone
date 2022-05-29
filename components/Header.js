3ce9b776dc63384fc1e15f68b6cb886173fc6909
garrettprince <89549339+garrettprince@users.noreply.github.com>
2022-05-28 17:56:43 -0700

    Basic start for header

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,26 @@
 import React from "react";
 import Image from "next/image";
+import { MenuIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
     <div>
-      <main className="flex">
-        <div className="flex h-10 w-10">
+      <main className="sticky top-0 z-50 grid grid-cols-2 bg-white">
+        <div
+          id="logo-container"
+          className="relative flex h-10 items-center m-6"
+        >
           <Image
             src="/assets/branding/full_logo_red.png"
             alt=""
             layout="fill"
             objectFit="contain"
+            objectPosition="left"
           />
         </div>
-        <div></div>
+        <div id="menu-container" className="flex justify-end items-center">
+          <MenuIcon className="h-10 w-10 m-6" />
+        </div>
       </main>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div>
+    <div className='mx-auto max-w-4xl'>
       <Head>
         <title>Pilot: Social Trip Planning</title>
         <link rel="" href="/assets/branding/pilot_logo.png" />
